0634c005b37eadeaff5533a0df710da7b0c36415
Amen Atikpo <alaskale2024@gmail.com>
2022-06-12 07:58:22 +0000

    function array incomplete

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+const returnFirstTwoDrivers =(drivers) => {
+       return drivers.slice(0, 2)
+};
+
+const returnLastTwoDrivers =(drivers) => {
+        return drivers.slice(-2);
+};
+
+const selectingDrivers =() =>{
+    returnFirstTwoDrivers();
+    returnLastTwoDrivers();
+    return returnFirstTwoDrivers().concat(returnLastTwoDrivers());
+};
