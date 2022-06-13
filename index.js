f67a99e9dd7ae82c1962bcc564e372437a42299d
Amen Atikpo <alaskale2024@gmail.com>
2022-06-13 17:04:23 +0000

    first class functions lab

--- index.js
+++ index.js
@@ -9,8 +9,21 @@ const returnLastTwoDrivers =(drivers) => {
         return drivers.slice(-2);
 };
 
-const selectingDrivers =() =>{
-    returnFirstTwoDrivers();
-    returnLastTwoDrivers();
-    return returnFirstTwoDrivers().concat(returnLastTwoDrivers());
-};
+const selectingDrivers=[returnFirstTwoDrivers,returnLastTwoDrivers];
+
+function createFareMultiplier(x){
+        return function multip(fare){
+                return fare*x;
+        }
+}
+function fareDoubler(fare){
+        return fare*2;
+     }
+function fareTripler(fare){
+        return fare*3;
+}
+function selectDifferentDrivers(arrayOfDrivers, func){
+     const arr = [...func]
+    return arr;
+
+}
